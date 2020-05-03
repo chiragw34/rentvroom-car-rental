48718d70e3e60152a1c8ffbc38cd867ed8a4bd5a
Chirag Wadhwa <chiragw34@gmail.com>
2020-05-03 11:03:13 +0530

    fixed hosting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // react
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 // CSS
 import "./App.css";
